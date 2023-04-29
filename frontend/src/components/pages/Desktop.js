10c0a1191dcf8589498a21d36d192d0229034c5a
Zongi <67127030+zongii@users.noreply.github.com>
2023-04-29 03:01:12 +0200

    Windows prototype

--- frontend/src/components/pages/Desktop.js
+++ frontend/src/components/pages/Desktop.js
@@ -18,12 +18,23 @@ function Desktop(props) {
             <div onClick={handleDarkTheme} className={styles.logo} />
 
             <div className={styles.workArea}>
+                <div className={`${styles.windowFrame}`}>
+                    <div className={styles.windowBar}>
 
+                    </div>
+                    <div className={styles.windowContent}>
+                        
+                    </div>
+                </div>
             </div>
 
             <div className={styles.taskbar}>
-                <div className={styles.taskbarLeftModal} tabIndex="0"></div>
-                <div className={styles.taskbarRightModal} tabIndex="0"></div>
+                <div className={styles.taskbarLeftModal} tabIndex="0">
+
+                </div>
+                <div className={styles.taskbarRightModal} tabIndex="0">
+
+                </div>
                 <div className={styles.taskbarIcons}>
                     <div style={{'--icon': `url(${logo})`}} className={`${styles.taskbarIcon} ${styles.menuIcon}`} tabIndex="0"></div>
                     <div style={{'--icon': `url(${icon1})`}} className={styles.taskbarIcon}></div>
